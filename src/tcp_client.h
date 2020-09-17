fcdccc10ae462ef0f2795726e6368eaef0ba7ddd
Philip Lundrigan <philiplundrigan@gmail.com>
2020-09-17 09:57:07 -0600

    Add specification for function pointer

--- src/tcp_client.h
+++ src/tcp_client.h
@@ -18,8 +18,7 @@
 #define TCP_CLIENT_DEFAULT_PORT "8080"
 #define TCP_CLIENT_DEFAULT_HOST "localhost"
 
-// Contains all of the information needed to create to connect to the server and
-// send it a message.
+// Contains all of the information needed to create to connect to the server and send it a message.
 typedef struct Config {
     char *port;
     char *host;
@@ -36,20 +35,20 @@ Config tcp_client_parse_arguments(int argc, char *argv[]);
 ///////////// SOCKET RELATED FUNCTIONS /////////////
 ////////////////////////////////////////////////////
 
-// Creates a TCP socket and connects it to the specified host and port. It
-// returns the socket file descriptor.
+// Creates a TCP socket and connects it to the specified host and port. It returns the socket file
+// descriptor.
 int tcp_client_connect(Config config);
 
-// Using the the action and message provided by the command line, format the
-// data to follow the protocol and send it to the server. Return 1 (EXIT_FAILURE) if an error
-// occurs, otherwise return 0 (EXIT_SUCCESS).
+// Using the the action and message provided by the command line, format the data to follow the
+// protocol and send it to the server. Return 1 (EXIT_FAILURE) if an error occurs, otherwise return
+// 0 (EXIT_SUCCESS).
 int tcp_client_send_request(int sockfd, char *action, char *message);
 
-// Receive the response from the server. The caller must provide a function
-// pointer that handles the response. After the response is handled by the
-// handle_response function pointer, the response data can be safely deleted.
-// The string passed to the function pointer must be null terminated. Return 1 (EXIT_FAILURE) if an
-// error occurs, otherwise return 0 (EXIT_SUCCESS).
+// Receive the response from the server. The caller must provide a function pointer that handles the
+// response and returns a true value if all responses have been handled, otherwise it returns a
+// false value. After the response is handled by the handle_response function pointer, the response
+// data can be safely deleted. The string passed to the function pointer must be null terminated.
+// Return 1 (EXIT_FAILURE) if an error occurs, otherwise return 0 (EXIT_SUCCESS).
 int tcp_client_receive_response(int sockfd, int (*handle_response)(char *));
 
 // Close the socket when your program is done running.
@@ -62,11 +61,10 @@ void tcp_client_close(int sockfd);
 // Given a file name, open that file for reading, returning the file object.
 FILE *tcp_client_open_file(char *file_name);
 
-// Gets the next line of a file, filling in action and message. This function
-// should be similar design to getline() (https://linux.die.net/man/3/getline).
-// *action and message must be allocated by the function and freed by the
-// caller.* When this function is called, action must point to the action string
-// and the message must point to the message string.
+// Gets the next line of a file, filling in action and message. This function should be similar
+// design to getline() (https://linux.die.net/man/3/getline). *action and message must be allocated
+// by the function and freed by the caller.* When this function is called, action must point to the
+// action string and the message must point to the message string.
 int tcp_client_get_line(FILE *fd, char **action, char **message);
 
 // Close the file when your program is done with the file.
